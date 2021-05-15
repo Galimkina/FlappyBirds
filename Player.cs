0320151c26267ad150db5dadb7c18f037acddeaa
Nastya Galimova <myEmail>
2021-05-15 15:35:46 +0500

    Добавила класс Игрок

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FlappyBird
+{
+    class Player
+    {
+        public int x;
+        public int y;
+        public Image birdImg;
+        public int size;
+
+        public Player(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+            birdImg = new Bitmap("C:\\Users\\vikap\\OneDrive\\Рабочий стол\\bird.png");
+            size = 49;
+        }
+    }
+}
