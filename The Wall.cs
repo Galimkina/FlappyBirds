8eabc5102af09ddb1a13b52e8f333217f5868785
Nastya Galimova <myEmail>
2021-05-15 15:36:10 +0500

    Добавила класс TheWall

---
+++ The Wall.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FlappyBird
+{
+    class The_Wall
+    {
+        public int x;
+        public int y;
+        public Image wallImg;
+        public int sizeX;
+        public int sizeY;
+
+        public The_Wall(int x, int y)
+        {
+            wallImg = new Bitmap("C:\\Users\\vikap\\OneDrive\\Рабочий стол\\tube.png");
+            this.x = x;
+            this.y = y;
+            sizeX = 49;
+            sizeY = 250;
+        }
+    }
+}
